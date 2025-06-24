e83139505ed7514c494c4f8ec120aa8b71c48dee
beme0815 <beate.m.e.koch@gmail.com>
2025-06-24 07:40:39 +0200

    api integration city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+// make API call and update interface
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "6b2a1c4tc5faa3abb9456dd906db1oe9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+//change searched city in h1
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
